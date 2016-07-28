c1413d983d28d123347e31c64c260c1fef08c860
sibrendebast <debastsibren@gmail.com>
2016-07-28 16:47:28 +0200

    Added comments on each bit of code

--- SmartBoard.py
+++ SmartBoard.py
@@ -1,3 +1,13 @@
+## Author: Sibren De Bast
+## in assignment of Easics,
+## July 2016
+
+###############################################################################
+##########                                                         ############
+##########                      DEPENDENCIES                       ############
+##########                                                         ############
+###############################################################################
+
 import socket
 import threading
 import time
@@ -10,7 +20,11 @@ from pymouse import PyMouse
 from scipy.optimize import fsolve
 import pygame
 
-
+###############################################################################
+##########                                                         ############
+##########                CONSTANTS AND VARIABLES                  ############
+##########                                                         ############
+###############################################################################
 
 global connections
 global angles
@@ -19,6 +33,7 @@ global width
 global heigth
 global refresh_interval
 
+## the rate at which the information is updated
 refresh_interval = 0.01
 
 
@@ -26,14 +41,15 @@ refresh_interval = 0.01
 angles = [float('nan'),float('nan'),float('nan'),float('nan')]
 coordinate = (float('nan'),float('nan'))
 
-screen_width = 1920
-screen_height = 1080
-width = screen_width
-height = screen_height
-
 
 # A list containing all the active server connections
 connections = []
+
+###############################################################################
+##########                                                         ############
+##########                     CLASS DEFINITIONS                   ############
+##########                                                         ############
+###############################################################################
 
 #####    Class to hangle all the networking trafic
 class ServerThread(threading.Thread):
@@ -177,6 +193,12 @@ class CoordinateThread(threading.Thread):
             coordinate = (C[0][0][0],C[0][1][0])
         return coordinate
 
+###############################################################################
+##########                                                         ############
+##########                     HELPER FUNCTIONS                    ############
+##########                                                         ############
+###############################################################################
+
 #######          calibration
 global delta_angles
 global delta_x
@@ -193,9 +215,11 @@ delta_y_list = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 nb_vert_pnts = 1
 nb_horz_pnts = 3 #HAS TO STAY 3!!!!
 
+
+## function to calibrate the setup
 def calibrate():
     pygame.init()
-    screen = pygame.display.set_mode((1920,1080),pygame.FULLSCREEN)
+    screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
     width, height = screen.get_size()
     pygame.display.set_caption('Calibration')
     pygame.mouse.set_visible(False)
@@ -208,15 +232,14 @@ def calibrate():
 
     
     for j in range(1,nb_vert_pnts+1):
-        #print 'j', j
         for i in range(1,(nb_horz_pnts+1)):
-            #print 'i', i
+            ## draw a circle with a cross on the screen to calibrate the system
             screen.fill((0,0,0))
             pygame.draw.circle(screen, white, (i*width/(nb_horz_pnts+1),j*height/(nb_vert_pnts+1)), 25)
             pygame.draw.lines(screen, red, False, [(i*width/(nb_horz_pnts+1)-30,j*height/(nb_vert_pnts+1)),(i*width/(nb_horz_pnts+1)+30,j*height/(nb_vert_pnts+1))], 3)
             pygame.draw.lines(screen, red, False, [(i*width/(nb_horz_pnts+1),j*height/(nb_vert_pnts+1)-30),(i*width/(nb_horz_pnts+1),j*height/(nb_vert_pnts+1)+30)], 3)
             pygame.display.update()
-            # wait until there is a pen detected
+            ## wait until there is a pen detected
             while str(angles[0]) == 'nan' or str(angles[1]) == 'nan' or str(angles[2]) == 'nan':
                 try:
                     time.sleep(0.05)
@@ -224,29 +247,32 @@ def calibrate():
                     pygame.quit()
                     raise
 
-            #wait until we have enough data points
-            
-            data = [[],[],[],[]]
+            ## initialize the array to store the data
+            data = [[],[],[],[]] 
+            ## wait until we have enough data points
             while len(data[0]) < 40:
                 try:
+                    ## draw a circle with a cross on the screen to calibrate the system, circle becomes smaller until enough data gathered
                     screen.fill((0,0,0))
                     pygame.draw.circle(screen, white, (i*width/(nb_horz_pnts+1),j*height/(nb_vert_pnts+1)), int(25-0.5*len(data[0])))
                     pygame.draw.lines(screen, red, False, [(i*width/(nb_horz_pnts+1)-30,j*height/(nb_vert_pnts+1)),(i*width/(nb_horz_pnts+1)+30,j*height/(nb_vert_pnts+1))], 3)
                     pygame.draw.lines(screen, red, False, [(i*width/(nb_horz_pnts+1),j*height/(nb_vert_pnts+1)-30),(i*width/(nb_horz_pnts+1),j*height/(nb_vert_pnts+1)+30)], 3)
                     pygame.display.update()
+                    ## gather data if the pen is detected
                     if (not str(angles[0]) == 'nan' and not str(angles[1]) == 'nan' and not str(angles[2]) == 'nan'):
                         data[0].append(angles[0]) #theta
                         data[1].append(angles[1]) #phi
                         data[2].append(angles[2]) #alpha
                         data[3].append(angles[3]) #beta
-                    time.sleep(0.05)
+                    time.sleep(0.025)
                 except:
                     pygame.quit()
                     raise
 
-            # wait until the pen is gone
+            ## wait until the pen is gone
             while not(str(angles[0]) == 'nan' or str(angles[1]) == 'nan' or str(angles[2]) == 'nan'):
                 try:
+                    ## Turn the cross green to let the user know we can move on
                     screen.fill((0,0,0))
                     pygame.draw.lines(screen, green, False, [(i*width/(nb_horz_pnts+1)-30,j*height/(nb_vert_pnts+1)),(i*width/(nb_horz_pnts+1)+30,j*height/(nb_vert_pnts+1))], 3)
                     pygame.draw.lines(screen, green, False, [(i*width/(nb_horz_pnts+1),j*height/(nb_vert_pnts+1)-30),(i*width/(nb_horz_pnts+1),j*height/(nb_vert_pnts+1)+30)], 3)
@@ -255,14 +281,14 @@ def calibrate():
                 except:
                     pygame.quit()
                     raise
-            #print data
+            ## compute the average of the collected angles to use them for the calibration
             calibration_angles[i-1] = [sum(data[0])/len(data[0]),sum(data[1])/len(data[1]),sum(data[2])/len(data[2]),sum(data[3])/len(data[3])]
             print calibration_angles[i-1]
                 
-        #print calibration_angles
+        
         
 
-        # theta
+        ## theta, calulation for the theta camera
         x1, y1, theta1 =   width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[0][0]
         x2, y2, theta2 = 2*width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[1][0]
         x3, y3, theta3 = 3*width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[2][0]
@@ -276,7 +302,7 @@ def calibrate():
         delta_angles_list[j-1][0],delta_x_list[j-1][0],delta_y_list[j-1][0]=fsolve(equations_theta, (0,50,100))[:]
         print delta_angles_list[j-1][0],delta_x_list[j-1][0],delta_y_list[j-1][0]
 
-        # phi
+        # phi, calulation for the phi camera
         x1, y1, theta1 =   width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[0][1]
         x2, y2, theta2 = 2*width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[1][1]
         x3, y3, theta3 = 3*width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[2][1]
@@ -290,7 +316,7 @@ def calibrate():
         delta_angles_list[j-1][1],delta_x_list[j-1][1],delta_y_list[j-1][1]=fsolve(equations_phi, (0,50,100))[:]
         print delta_angles_list[j-1][1],delta_x_list[j-1][1],delta_y_list[j-1][1]
 
-        # alpha
+        # alpha, calulation for the alpha camera
         x1, y1, theta1 =   width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[0][2]
         x2, y2, theta2 = 2*width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[1][2]
         x3, y3, theta3 = 3*width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[2][2]
@@ -304,7 +330,7 @@ def calibrate():
         delta_angles_list[j-1][2],delta_x_list[j-1][2],delta_y_list[j-1][2]=fsolve(equations_alpha, (0,50,100))[:]
         print delta_angles_list[j-1][2],delta_x_list[j-1][2],delta_y_list[j-1][2]
 
-        # beta
+        # beta, calulation for the beta camera
         x1, y1, theta1 =   width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[0][3]
         x2, y2, theta2 = 2*width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[1][3]
         x3, y3, theta3 = 3*width/(nb_horz_pnts+1), j*height/(nb_vert_pnts+1), calibration_angles[2][3]
@@ -428,7 +454,7 @@ U = zeros((X.shape[0],1))
 
 Y = array([[0],[0]])
 H = array([[1,0,0,0],[0,1,0,0]])
-R = eye(Y.shape[0])*7
+R = eye(Y.shape[0])*5
 
 # flag to keep the state of the mouse
 pressed = False
@@ -436,26 +462,29 @@ pressed = False
 
 while True:
     try:
+        ## ask coordinate
         coordinate = coorCalculator.calc_coor()
-        #print angles[0]
+        ## check if valid coordinate
         if not math.isnan(coordinate[0]):
+            ## put the coordinate in a numpy array
             Y = array([[coordinate[0]],[coordinate[1]]])
+            ## apply Kalman filter on coordinates
             (X,P) = kf_predict(X,P,A,Q,B,U)
             (X,P,K,IM,IS,LH) = kf_update(X, P, Y, H, R)
-            # move and press the mouse
-            #mouse.move((X[:2]).tolist()[0][0],(X[:2]).tolist()[1][0])
+            ## move and press the mouse
             mouse.press((X[:2]).tolist()[0][0],(X[:2]).tolist()[1][0])
             pressed = True
         else:
+            ## if the pen is reoved we reinitialze the the matrices from the Kalman filter
             x = np.matrix('0. 0. 0. 0.').T 
             P = np.matrix(np.eye(4))*1000 # initial uncertainty
-            # release the mouse
+            ## release the mouse
             if pressed:
                 mouse.release((X[:2]).tolist()[0][0],(X[:2]).tolist()[1][0])
                 pressed = False
-            
+        ## sleep for a while 
         time.sleep(refresh_interval)
-    # if we get a keyboard interrupt, kill all the running threads.
+    ## if we get a keyboard interrupt, kill all the running threads.
     except KeyboardInterrupt:
         connHandler.stop()
         syncThread.stop()
--- track_IR.py
+++ track_IR.py
@@ -1,4 +1,19 @@
-#####           Import necessary packages      #####
+## Script to read out the camera of a raspberry pi and and calculate the angle
+## at which an IR pen is located in relation to the raspberry pi. The raspberry
+## pi is fitted with an IR camera (version 2). The program listens to incoming
+## connections to send out the measured angle.
+##
+## Author: Sibren De Bast
+## in assignment of Easics,
+## July 2016
+
+
+###############################################################################
+##########                                                         ############
+##########                      DEPENDENCIES                       ############
+##########                                                         ############
+###############################################################################
+
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
@@ -6,19 +21,32 @@ import threading
 import numpy as np
 import socket
 from threading import Thread
-
                   
-#####                  CONSTANTS                #####
+###############################################################################
+##########                                                         ############
+##########                 CONSTANTS AND VARIABLES                 ############
+##########                                                         ############
+###############################################################################
+
 TCP_IP = 'digitalboard1'
 TCP_PORT = 12345
 BUFFER_SIZE = 1024
 
+## resolution of the camera
 WIDTH = 1296
 HEIGHT = 736
+## How much of the sides we don't need
 SIDEWIDTH = WIDTH / 8
 
+## variable to hold the angle at which the pen is located
 global theta
 theta = float('nan')
+
+###############################################################################
+##########                                                         ############
+##########                     CLASS DEFINITIONS                   ############
+##########                                                         ############
+###############################################################################
 
 #####   class to put video recording on seperate thread   #####
 class PiVideoStream:
@@ -29,7 +57,7 @@ class PiVideoStream:
         self.camera.framerate = framerate
         self.rawCapture = PiRGBArray(self.camera, size=resolution)
         self.stream = self.camera.capture_continuous(self.rawCapture,
-                format="bgr", use_video_port=True)
+                format="rgb", use_video_port=True)
 
         # initialize the frame and the variable used to indicate
         # if the thread should be stopped
@@ -71,6 +99,7 @@ class ClientThread(threading.Thread):
     
 
     def __init__(self,ip,port):
+        ## initialize the thread
         threading.Thread.__init__(self)
         self.ip = ip
         self.port = port
@@ -80,44 +109,52 @@ class ClientThread(threading.Thread):
     def run(self):
         while not self.stopped:
             try:
+                ## connect to a remote host
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                #print 'connecting....'
                 s.connect((self.ip, self.port))
                 while not self.stopped:
+                        ## receive a sync signal 
                         received = s.recv(128)
-                        #print received
+                        ## when receiving the right signal, send back the angle
                         if received.split()[0] == 'request':
-                            #print received.split()[1]
                             s.send(received.split()[1]+' '+str(theta))
                 s.close()
             except:
-                #print 'connection failed'
+                ## wait some time before trying to reconnect (in case connection fails a lot)
                 time.sleep(0.1)
                 pass
         
 
     def stop(self):
+        ## stop the thread
         self.stopped = True
 
+###############################################################################
+##########                                                         ############
+##########                            MAIN                         ############
+##########                                                         ############
+###############################################################################
+        
 
+## Start the communication
 client = ClientThread('10.0.7.119',12345)
 client.start()
 
-#####      Capture frames from the camera        #####
+##      Capture frames from the camera     
 vs = PiVideoStream().start()
+
+## Give the camera some time to start
 time.sleep(0.5)
 
 try:
     while True:
         image = vs.read()
+        ## crop the image to the needed size
         image = image[300:400, SIDEWIDTH:WIDTH-SIDEWIDTH,0]
-        
         
         if image.max() > 220:
             x=np.unravel_index(image.argmax(),image.shape)[1]
-            #theta = 148*(float(x)+SIDEWIDTH)/WIDTH-26.245
             theta = 90-(131.1*(float(x)+SIDEWIDTH)/WIDTH-20.642)
-            #print theta
         else:
             theta = float('nan')
         
@@ -127,14 +164,5 @@ try:
         
 except KeyboardInterrupt:
     vs.stop()
-    client.stop()
-
-
-
-
-
-
-
-
-    
+    client.stop()    
 
